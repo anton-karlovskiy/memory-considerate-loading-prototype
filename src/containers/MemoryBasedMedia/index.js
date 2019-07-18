ae048bb77dbc00f5d003a7eabf06cfe1791492d7
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-07-18 12:34:58 +0300

    handled memory status API not supported case

--- src/containers/MemoryBasedMedia/index.js
+++ src/containers/MemoryBasedMedia/index.js
@@ -11,7 +11,7 @@ const MemoryBasedMedia = () => {
   console.log('[MemoryBasedMedia] memoryStatus => ', memoryStatus);
   if (!memoryStatus) return <Fragment>Loading...</Fragment>;
 
-  const { usedMemoryPercent } = memoryStatus
+  const { usedMemoryPercent } = memoryStatus;
   let media = null;
   switch(true) {
     case usedMemoryPercent > 75:
@@ -21,7 +21,12 @@ const MemoryBasedMedia = () => {
       media = <ModelViewer />;
       break;
     default:
-      media = <video className='responsive' src='https://cdn.glitch.com/8d7fb7f0-a9be-4a8c-96c7-8af286af487e%2F4g-video.mp4?v=1562842601068' controls />;
+      media = (
+        <Fragment>
+          <span>{memoryStatus}</span>
+          <ModelViewer />
+        </Fragment>
+      );
       break;
   }
 

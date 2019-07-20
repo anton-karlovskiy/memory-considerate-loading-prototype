649587ac9826894f4ea83fc63a8679e4a2cbda6a
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-07-20 23:25:55 +0300

    updated naming from based to considerate

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import MemoryBasedMedia from './containers/MemoryBasedMedia';
+import MemoryConsiderateMedia from './containers/MemoryConsiderateMedia';
 
 import './App.css';
 
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <MemoryBasedMedia />
+        <MemoryConsiderateMedia />
       </header>
     </div>
   );
